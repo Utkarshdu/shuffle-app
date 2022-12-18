017671d9e74cab3e312d4a4b0c15134c3a57299a
Utkarsh Dubey <dubeyu746@gmail.com>
2022-12-19 01:32:57 +0530

    added database config and initialzation

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const app= express();
+const mongoose= require("mongoose");
+
+const db= 'mongodb+srv://utk123:Sz5XnB2CvOybszy6@cluster0.rqrlqgg.mongodb.net/shuffle-app-db?retryWrites=true&w=majority'
+mongoose.set("strictQuery", true);
+mongoose.connect(db, {
+    
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindandModify: false
+}).then(() =>{
+    console.log('connection succesful')
+}).catch((error)=> console.log(`no connection`));
 
 const port=process.env.port|| 3000;
 const host=process.env.host|| "localhost"
