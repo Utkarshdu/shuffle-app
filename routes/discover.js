6e96d731b396312f8f85df26c63008166c23b633
Utkarsh Dubey <dubeyu746@gmail.com>
2022-12-19 15:56:44 +0530

    added list category api

---
+++ routes/discover.js
@@ -0,0 +1,34 @@
+const { Router } = require("express");
+const route = Router();
+const CategoryModel = require("../database-model/category");
+const GalleryModel = require("../database-model/image-gallery");
+
+route.get("/like/:imageId", async (req, res, next) => {
+    try {
+        const imageId = req.params.imageId;
+        var likesNumber;
+        const imageDetail = await GalleryModel.findOne({ _id: imageId });
+
+        if (imageDetail) {
+            if (imageDetail.likes) {
+                likesNumber = 0;
+            } else {
+                likesNumber = 1;
+            }
+        }
+
+        await GalleryModel.updateOne(
+            { _id: imageId },
+            { $set: { likeNumber: likesNumber } }
+        );
+
+        
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+});
+
+route.get("/discover/:category", async(req,res,next)=>{
+
+});
